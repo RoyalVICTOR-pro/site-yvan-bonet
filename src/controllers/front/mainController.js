5f603c8c140024b46b4a971bf4e66413ab977a25
Royal VICTOR <rvictor@cbi-multimedia.com>
2023-04-30 21:20:23 +0200

    Pages domaines + Liens Footer + Liens vers Domaines

--- src/controllers/front/mainController.js
+++ src/controllers/front/mainController.js
@@ -1,8 +1,20 @@
-const seoData = require('../../data/seo.json');
+const contentData = require('../../data/content.json');
+// const exphbs = require('express-handlebars');
 
 exports.getHomePage = (req, res) => {
   console.log('Entrée dans getHomePage');
+  // exphbs.registerPartial('')
   res.status(200).render('front/home', {
-    title: seoData.home.title,
+    title: contentData.home.seo_title
+  });
+};
+
+exports.getDomainPage = (req, res) => {
+  res.status(200).render('front/domaine', {
+    seo_title: contentData[req.params.slug].seo_title,
+    icon_file_name: contentData[req.params.slug].icon_file_name,
+    page_title: contentData[req.params.slug].page_title,
+    page_intro: contentData[req.params.slug].page_intro,
+    page_text: contentData[req.params.slug].page_text,
   });
 };
--- src/routes/frontRoutes.js
+++ src/routes/frontRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.get('/', frontMainController.getHomePage);
 // router.get('/domaine-intervention/:slug', viewsController.getTour);
 
+router.get('/domaines-interventions/:slug/', frontMainController.getDomainPage);
+
 module.exports = router;
