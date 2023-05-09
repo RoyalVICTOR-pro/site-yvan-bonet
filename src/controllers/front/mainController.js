b10d44b809318e64c3428c8a8115eacace93fe57
Royal VICTOR <rvictor@cbi-multimedia.com>
2023-05-09 23:29:12 +0200

    Montage BDD et Page vide Contact

--- src/controllers/front/mainController.js
+++ src/controllers/front/mainController.js
@@ -23,37 +23,46 @@ exports.getDomainPage = (req, res) => {
 exports.getPage = (pageName) => {
   return (req, res) => {
     switch (pageName) {
-      case 'mentions-legales':
-      case 'politique-confidentialite':
-        res.status(200).render('front/legalPage', {
-          active_menu: contentData[pageName].active_menu,
-          title: contentData[pageName].seo_title,
-          page_title: contentData[pageName].page_title,
-          page_text: contentData[pageName].page_text
-        });
-        break;
-      case 'parcours':
-        res.status(200).render('front/parcoursPage', {
-          active_menu: contentData[pageName].active_menu,
-          title: contentData[pageName].seo_title,
-          icon_file_name: contentData[pageName].icon_file_name,
-          page_title: contentData[pageName].page_title,
-          page_intro: contentData[pageName].page_intro,
-          page_text: contentData[pageName].page_text
-        });  
-        break;
-      case 'honoraires':
-        res.status(200).render('front/mainInternalPage', {
-          active_menu: contentData[pageName].active_menu,
-          title: contentData[pageName].seo_title,
-          icon_file_name: contentData[pageName].icon_file_name,
-          page_title: contentData[pageName].page_title,
-          page_intro: contentData[pageName].page_intro,
-          page_text: contentData[pageName].page_text
-        });  
-        break;
-      default:
-        break;
+    case 'mentions-legales':
+    case 'politique-confidentialite':
+      res.status(200).render('front/legalPage', {
+        active_menu: contentData[pageName].active_menu,
+        title: contentData[pageName].seo_title,
+        page_title: contentData[pageName].page_title,
+        page_text: contentData[pageName].page_text
+      });
+      break;
+    case 'parcours':
+      res.status(200).render('front/parcoursPage', {
+        active_menu: contentData[pageName].active_menu,
+        title: contentData[pageName].seo_title,
+        icon_file_name: contentData[pageName].icon_file_name,
+        page_title: contentData[pageName].page_title,
+        page_intro: contentData[pageName].page_intro,
+        page_text: contentData[pageName].page_text
+      });  
+      break;
+    case 'honoraires':
+      res.status(200).render('front/mainInternalPage', {
+        active_menu: contentData[pageName].active_menu,
+        title: contentData[pageName].seo_title,
+        icon_file_name: contentData[pageName].icon_file_name,
+        page_title: contentData[pageName].page_title,
+        page_intro: contentData[pageName].page_intro,
+        page_text: contentData[pageName].page_text
+      });  
+      break;
+    default:
+      break;
     }
   };
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('front/contact', {
+    active_menu: contentData.contact.active_menu,
+    title: contentData.contact.seo_title,
+    icon_file_name: contentData.contact.icon_file_name,
+    page_title: contentData.contact.page_title
+  });
 };
---
+++ src/sequelizeUtils/migrations/20230505125423-create-contact.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      prenom: {
+        type: Sequelize.STRING
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      telephone: {
+        type: Sequelize.STRING
+      },
+      message: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contacts');
+  }
+};
