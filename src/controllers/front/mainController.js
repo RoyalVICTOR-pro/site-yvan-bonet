6c436906b1f8eb95c66acde13c94946052185d05
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-06-24 14:29:55 +0200

    CSP Management Suite

--- src/controllers/front/mainController.js
+++ src/controllers/front/mainController.js
@@ -65,6 +65,9 @@ exports.getPage = (pageName) => {
   };
 };
 exports.getLandingPage = (pageName) => {
+  const crypto = require('crypto');
+  const nonce = crypto.randomBytes(16).toString('base64');
+
   return (req, res) => {
     switch (pageName) {
     case 'droit-penal':
@@ -77,7 +80,8 @@ exports.getLandingPage = (pageName) => {
         page_subtitle: contentData[pageName].page_subtitle,
         text_cabinet: contentData['cabinet'].page_text,
         contact_page_title: contentData['contact'].page_title,
-        contact_icon_file_name : contentData['contact'].icon_file_name,
+        contact_icon_file_name: contentData['contact'].icon_file_name,
+        nonce: nonce
       });
       break;
     default:
