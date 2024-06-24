5877e704b75b76daf2b1d003837b6c957ff340b7
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-06-24 15:52:32 +0200

    CSP Management Suite

--- app.js
+++ app.js
@@ -44,24 +44,23 @@ app.set('views', path.join(__dirname, 'src/views/'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+/* app.use(helmet());
 
 app.use((req, res, next) => {
   const nonce = crypto.randomBytes(16).toString('hex');
   req.nonce = nonce;
-  res.locals.nonce = nonce;
 
   next();
-});
+}); */
 
-app.use(
+/* app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      scriptSrc: ['\'self\'', (req, res) => `'nonce-${res.locals.nonce}'`],
-      imgSrc: ['\'self\'', 'www.googletagmanager.com'],
+      scriptSrc: ["'self'", `'nonce-${req.nonce}'`],
+      imgSrc: ["'self'", 'www.googletagmanager.com'],
     },
   })
-);
+); */
 
 // Developping logging
 if (process.env.NODE_ENV === 'development') {
