2ae1e52427f38b481538a770bfbe047f4d5e4e8d
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-07-09 09:52:46 +0200

    Tag supplémentaire Google

--- src/controllers/front/mainController.js
+++ src/controllers/front/mainController.js
@@ -131,6 +131,7 @@ exports.googleAnalyticsMiddleware = (req, res, next) => {
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', '${res.locals.googleAnalyticsTrackingId}');
+        gtag('config', 'AW-16614052928’);
       </script>
     `;
   } else {
